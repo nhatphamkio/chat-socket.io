cfb58f1af8b81f5ab768fb1f39050e95e01a1f88
Owner <owner@Owner-no-MacBook-Pro.local>
2020-07-01 22:00:44 +0900

    initial commit

--- index.js
+++ index.js
@@ -8,7 +8,14 @@ var io = require("socket.io")(server);
 server.listen(3000);
 //on listen
 io.on("connection",function(socket){
-    console.log("co nguoi ket noi");
+    console.log("co nguoi ket noi" + socket.id);
+    socket.on("disconnect",function(){
+        console.log(socket.id + " huy ket noi!!!");
+    })
+    socket.on("Client-send-data",function(data){
+        console.log(socket.id+" vua gui "+data);
+        io.sockets.emit("Server-send-data",data+"888");
+    });
 });
 app.get("/",function(req,res){
     res.render("trangchu");
